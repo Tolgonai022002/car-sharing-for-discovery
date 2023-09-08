545ebbc281016747c71dea6e6a8f6ee8f4c9eafa
Tolgonai022002 <tokaldomotova@gmail.com>
2023-09-07 17:11:07 -0900

    Remove sms-nikita

--- src/utils/is-phone-number.ts
+++
@@ -1,1 +0,0 @@
-export const isPhoneNumber = (phone) => /^[9][9][6]([0-9]{9})$/.test(phone);
--- src/utils/removePlusFromNumbers.ts
+++
@@ -1,6 +0,0 @@
-export function removePlusFromPhoneNumber(phoneNumber: string) {
-  if (phoneNumber[0] === '+') {
-    return phoneNumber.substr(1);
-  }
-  return phoneNumber;
-}
